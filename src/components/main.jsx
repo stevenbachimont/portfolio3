2881f8610142b184b0536bfbd7505682c501fd85
stevenbachimont <stevenbachimont@gmail.com>
2024-09-20 14:04:17 +0200

    améliorations diverses :-)

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -15,9 +15,7 @@ const Home = () => {
             <div className="container">
               <h5 className="card-title fs-1 text fw-lighter">New Arrivals</h5>
               <p className="card-text fs-5 d-none d-sm-block ">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+                Puisqu'il faut savoir se vendre, autant le faire vraiment.
               </p>
             </div>
           </div>
